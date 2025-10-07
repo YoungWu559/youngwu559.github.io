35d5be86f4d11eca42f7de6a4edadcf9edfdcc97
Young Wu <wu489@wisc.edu>
2025-10-07 15:52:06 -0500

    Update

--- CS559WB5/for_students/05-09-02.js
+++ CS559WB5/for_students/05-09-02.js
@@ -1,12 +1,62 @@
+/**
+ * CS559 Spring 2023 Example Solution
+ * Written by CS559 course staff
+ */
+
 // @ts-check
+/* jshint -W069, -W141, esversion:6 */
 export {};  // null statement to tell VSCode we're doing a module
 
-// draw a picture using curves!
+/**
+ * drawing function for box 2
+ * 
+ * draw a picture using curves!
+ **/
+/* no need for onload - we use defer */
 
-let canvas = document.getElementById("canvas1");
-if (!(canvas instanceof HTMLCanvasElement))
+/** @type {HTMLCanvasElement} */ const canvas = /** @type {HTMLCanvasElement} */ (document.getElementById("canvas1"));
+if (!(canvas instanceof HTMLCanvasElement)) {
     throw new Error("Canvas is not HTML Element");
+}
 
-
+// Begin Example Solution
+/** @type {CanvasRenderingContext2D} */ const context = canvas.getContext("2d");
+/**
+ * Generate random x coordinate
+ */
+function rand_x() {
+    return Math.random() * canvas.width;
+}
+/**
+ * Generate random y coordinate
+ */
+function rand_y() {
+    return Math.random() * canvas.height;
+}
+/**
+ * Generate random color component
+ */
+function rand_c() {
+    return Math.random() * 255;
+}
+// Start at top-left corner and draw random path using bezier curves
+/** @type {number[]} */ let [px0, py0, px1, py1, px2, py2] = [0, 0, 0, 0, 0, 0];
+for (let i = 0; i < 100; i ++) 
+{
+  context.beginPath();
+  context.strokeStyle = `rgb(${rand_c()}, ${rand_c()}, ${rand_c()})`; // CS559 Sample Code
+  context.moveTo(px0, py0);
+  px0 = rand_x();
+  py0 = rand_y();
+  px1 = rand_x();
+  py1 = rand_y();
+  context.bezierCurveTo(px2, py2, px1, py1, px0, py0);
+  px2 = 2 * px0 - px1;
+  py2 = 2 * py0 - py1;
+  context.stroke();
+}
+// End Example Solution
 
 // CS559 2025 Workbook
+
+// CS559 2025 Example Solution
--- CS559WB5/for_students/05-10-03.js
+++ CS559WB5/for_students/05-10-03.js
@@ -1,4 +1,19 @@
+/**
+ * CS559 Spring 2023 Example Solution
+ * Written by CS559 course staff
+ */
+
+/**
+ * 05-10-03.js - a simple JavaScript file that gets loaded with
+ * page 10 of Workbook 5 (CS559).
+ *
+ * written by Michael Gleicher, January 2019
+ * modified January 2020
+ *
+ */
+
 // @ts-check
+/* jshint -W069, esversion:6 */
 
 import { RunCanvas } from "../libs/CS559/runCanvas.js";
 
@@ -18,41 +33,50 @@ function myTri(t) {
 
 function myTriAL(t) {
   // students should write this!
-  return [0, 0];
+  // Begin Example Solution
+  let len1 = 130;
+  let len2 = 120;
+  let len3 = 50;
+  let st = t * (len1 + len2 + len3) / 3;
+  if (st < len1) return (lerpPt(triPt1, triPt2, st / len1));
+  else if (st < len1 + len2) return (lerpPt(triPt2, triPt3, (st - len1) / len2));
+  else return lerpPt(triPt3, triPt1, (st - len1 - len2) / len3);
+  // End Example Solution
 }
 
+/* no need for onload - we use defer */
 
 // note that checking that canvas is the right type of element tells typescript
 // that this is the right type - it's a form of a safe cast 
 let canvas = document.getElementById("canvas1");
 if (!(canvas instanceof HTMLCanvasElement))
-    throw new Error("Canvas is not HTML Element");
+  throw new Error("Canvas is not HTML Element");
 
 let context = canvas.getContext("2d");
 
 function draw(canvas, t) {
-    context.save();
-    context.clearRect(0, 0, canvas.width, canvas.height);
+  context.save();
+  context.clearRect(0, 0, canvas.width, canvas.height);
 
-    context.scale(2, 2);
+  context.scale(2, 2);
 
-    context.strokeStyle = "black";
-    context.lineWidth = 2;
-    context.beginPath();
-    context.moveTo(10, 10);
-    context.lineTo(10 + 120, 10 + 50);
-    context.lineTo(10, 10 + 50);
-    context.closePath();
-    context.stroke();
+  context.strokeStyle = "black";
+  context.lineWidth = 2;
+  context.beginPath();
+  context.moveTo(10, 10);
+  context.lineTo(10 + 120, 10 + 50);
+  context.lineTo(10, 10 + 50);
+  context.closePath();
+  context.stroke();
 
-    let [x, y] = myTri(t);
-    context.fillStyle = "red";
-    context.fillRect(x - 5, y - 5, 10, 10);
+  let [x, y] = myTri(t);
+  context.fillStyle = "red";
+  context.fillRect(x - 5, y - 5, 10, 10);
 
-    let [x2, y2] = myTriAL(t);
-    context.fillStyle = "green";
-    context.fillRect(x2 - 5, y2 - 5, 10, 10);
-    context.restore();
+  let [x2, y2] = myTriAL(t);
+  context.fillStyle = "green";
+  context.fillRect(x2 - 5, y2 - 5, 10, 10);
+  context.restore();
 }
 let rc = new RunCanvas(canvas, draw);
 rc.setupSlider(0, 3, 0.05);
@@ -60,3 +84,5 @@ rc.setValue(0);
 
 
 // CS559 2025 Workbook
+
+// CS559 2025 Example Solution
